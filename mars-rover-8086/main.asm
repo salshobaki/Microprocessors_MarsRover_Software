3f3282c65bd7647859362d9d132726de2f0aaf2c
Kareem AbuSharkh <kar20230995@std.psut.edu.jo>
2026-05-04 11:59:36 +0300

    Remove semester information and clean up code

--- mars-rover-8086/main.asm
+++ mars-rover-8086/main.asm
@@ -1,7 +1,6 @@
 ; ============================================
 ; Mars Rover Grid Navigator
 ; Princess Sumaya University for Technology
-; 22344 - Microprocessors | Spring 2026
 ; ============================================
 
 data segment
@@ -16,16 +15,17 @@ code segment
 INCLUDE sarah_map.inc
 INCLUDE kareem_input.inc
 INCLUDE abdulrhman_logic.inc
+
 start:
     mov ax, data
     mov ds, ax
     mov es, ax
 
+    CALL CHOOSE_MODE      
     CALL DRAW_GRID
     CALL GET_COMMANDS
     CALL RUN_SEQUENCE
-
-    ; wait for any key....    
+  
     mov ah, 1
     int 21h
     mov ax, 4c00h
